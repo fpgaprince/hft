a8d560ec9f3fbf207073e12047bb38ed3800236f
nibbles <mnguyend@gmail.com>
2023-12-24 13:04:11 -0800

    few more comments

--- feedhandler.vhd
+++ feedhandler.vhd
@@ -3,29 +3,29 @@ use ieee.std_logic_1164.all;
 
 package nasdaq_itch is
 
-  __ Outputs from the FIFO.
+  -- Outputs from the FIFO.
   type t_FROM_FIFO is record
-    wr_full  : std_logic;                __ FIFO Full Flag
-    rd_empty : std_logic;                __ FIFO Empty Flag
+    wr_full  : std_logic;                -- FIFO Full Flag
+    rd_empty : std_logic;                -- FIFO Empty Flag
     rd_dv    : std_logic;
     rd_data  : std_logic_vector(7 downto 0);
   end record t_FROM_FIFO;
 
-  __ Inputs to the FIFO.
+  -- Inputs to the FIFO.
   type t_TO_FIFO is record
     wr_en    : std_logic;
     wr_data  : std_logic_vector(7 downto 0);
     rd_en    : std_logic;
   end record t_TO_FIFO;
 
-  __ constant c_FROM_FIFO_INIT : t_FROM_FIFO := (wr_full => '0',
-  __                                             rd_empty => '1',
-  __                                             rd_dv => '0',
-  __                                             rd_data => (others => '0'));
+  -- constant c_FROM_FIFO_INIT : t_FROM_FIFO := (wr_full => '0',
+  --                                             rd_empty => '1',
+  --                                             rd_dv => '0',
+  --                                             rd_data => (others => '0'));
 
-  __ constant c_TO_FIFO_INIT : t_TO_FIFO := (wr_en => '0',
-  __                                         wr_data => (others => '0'),
-  __                                         rd_en => '0');
+  -- constant c_TO_FIFO_INIT : t_TO_FIFO := (wr_en => '0',
+  --                                         wr_data => (others => '0'),
+  --                                         rd_en => '0');
 
 
 end package nasdaq_itch;
@@ -46,15 +46,15 @@ end package nasdaq_itch;
 
 
 
-__ ascii has 128 char, 7bits req
-__ 1byte, 2hex, 8bits
+-- ascii has 128 char, 7bits req
+-- 1byte, 2hex, 8bits
 
-__ assume ethernet output data = 32bit, 4bytes
+-- assume ethernet output data = 32bit, 4bytes
 
 
-__ ethernet_input_tdata
+-- ethernet_input_tdata
 
-__ byte re_order when reassembling received bytes/data
+-- byte re_order when reassembling received bytes/data
 
   network_byte7_0 = eth_tdata_in[7:0];
   network_byte15_8 = eth_tdata_in[15:8];
@@ -64,17 +64,17 @@ __ byte re_order when reassembling received bytes/data
 
 host_byte_order = {network_byte7_0, network_byte15_8, network_byte23_16, network_byte31_24};
 
-__ meant that MSB of host word was transfered first!.
-__ we receive the MSB first and downwards
+-- meant that MSB of host word was transfered first!.
+-- we receive the MSB first and downwards
 
 
-__ The TCP/IP standard network byte order is big_endian.
-__ the lowest memory address to be the high_order bit, which is called big endian.
-__ MSB to LSB as usual when we assign on host machine,
-__ but network will pull the MSB to transfer first.
+-- The TCP/IP standard network byte order is big_endian.
+-- the lowest memory address to be the high_order bit, which is called big endian.
+-- MSB to LSB as usual when we assign on host machine,
+-- but network will pull the MSB to transfer first.
 
 
-__ just enumerate ASCII somewhere else =>
+-- just enumerate ASCII somewhere else =>
 
 stock_locate
 tracking_num
@@ -82,28 +82,35 @@ timestamp
 stock_symbol
 event_code
 
-__ https:--www.nasdaqtrader.com/content/technicalsupport/specifications/dataproducts/NQTVITCHSpecification.pdf
+-- https:--www.nasdaqtrader.com/content/technicalsupport/specifications/dataproducts/NQTVITCHSpecification.pdf
   case msg_type is
 
-    __ 1.1 System Event Message
+    -- 1.1 System Event Message
     when S =>
         stock_locate
         tracking_num
         timestamp
         event_code
 
-__ i dont know if we need this, they didn't have it. what determines what we need.
-__ 1.2 Stock Related Messages
-    __ 1.2.1 Stock Directory
-    __ when R =>
-    __   stock_locate
-    __   tracking_no
-    __   timestamp
-    __   stock_symbol
-    __   market_category
-    __   FinancialStatusIndicator
+        -- (83 system-event-message ; S
+        -- (locate uint16)
+        -- (tracking uint16)
+        -- (timestamp uint48)
+        -- (event-code uint8))
 
-    __ 1.2.2 Stock Trading Action
+
+-- i dont know if we need this, they didn't have it. what determines what we need.
+-- 1.2 Stock Related Messages
+    -- 1.2.1 Stock Directory
+    -- when R =>
+    --   stock_locate
+    --   tracking_no
+    --   timestamp
+    --   stock_symbol
+    --   market_category
+    --   FinancialStatusIndicator
+
+    -- 1.2.2 Stock Trading Action
     when H =>
       stock_locate
       tracking_num
@@ -111,16 +118,16 @@ __ 1.2 Stock Related Messages
       stock_symbol
       trading_state
 
-      __ no reserved or reason?
+      -- no reserved or reason?
 
-      (stock_locate uint16)
-      (tracking uint16)
-      (timestamp uint48)
-      (symbol uint64)
-      (trading_state uint8))
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (symbol uint64)
+      -- (trading_state uint8))
 
 
-    __ 1.2.3 Reg SHO Short Sale Price Test RestrictedIndicator
+    -- 1.2.3 Reg SHO Short Sale Price Test RestrictedIndicator
     when Y =>
       stock_locate
       tracking_num
@@ -128,38 +135,45 @@ __ 1.2 Stock Related Messages
       stock_symbol
       reg_sho_action
 
-__ i dont know if we need this, they didn't have it. what determines what we need.
-    __ 1.2.4 Market Participant Position
+      -- (89 reg-sho ; A
+      -- (locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (symbol uint64)
+      -- (reg-sho-action uint8))
 
-    __ when L =>
-    __   stock_locate
-    __   tracking_no
-    __   timestamp
-    __   stock_symbol
-    __   reg_sho_action
+-- i dont know if we need this, they didn't have it. what determines what we need.
+    -- 1.2.4 Market Participant Position
 
-__ __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.2.5 Market___Wide Circuit Breaker (MWCB) Messaging
-__     when V =>
-__       code for this branch
+    -- when L =>
+    --   stock_locate
+    --   tracking_no
+    --   timestamp
+    --   stock_symbol
+    --   reg_sho_action
 
-__ __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.2.6 Quoting Period Update
-__     when K =>
-__       code for this branch
+-- -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.2.5 Market--_Wide Circuit Breaker (MWCB) Messaging
+--     when V =>
+--       code for this branch
 
-__ __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.2.7 Limit Up – Limit Down (LULD) Auction Collar
-__     when J =>
-__       code for this branch
+-- -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.2.6 Quoting Period Update
+--     when K =>
+--       code for this branch
 
-__       __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.2.8 Operational Halt
-__     when h =>
-__       code for this branch
+-- -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.2.7 Limit Up – Limit Down (LULD) Auction Collar
+--     when J =>
+--       code for this branch
 
-__ 1.3 Add Order Message
-    __ 1.3.1 Add Order – No MPID Attribution
+--       -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.2.8 Operational Halt
+--     when h =>
+--       code for this branch
+
+-- 1.3 Add Order Message
+    -- 1.3.1 Add Order – No MPID Attribution
     when A =>
       stock_locate
       tracking_num
@@ -171,18 +185,18 @@ __ 1.3 Add Order Message
       price
 
 
-      __ (65 add_order ; A
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64)
-      __ (buy_sell uint8)
-      __ (num_shares uint32)
-      __ (symbol uint64)
-      __ (price uint32))
+      -- (65 add_order ; A
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64)
+      -- (buy_sell uint8)
+      -- (num_shares uint32)
+      -- (symbol uint64)
+      -- (price uint32))
 
 
-    __ 1.3.2 Add Order with MPID Attribution
+    -- 1.3.2 Add Order with MPID Attribution
     when F =>
       stock_locate
       tracking_num
@@ -194,20 +208,20 @@ __ 1.3 Add Order Message
       price
       attribution
 
-      __ (70 add_order_with_mpid ; F
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64)
-      __ (buy_sell uint8)
-      __ (num_shares uint32)
-      __ (symbol uint64)
-      __ (price uint32)
-      __ (attribution uint32))
+      -- (70 add_order_with_mpid ; F
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64)
+      -- (buy_sell uint8)
+      -- (num_shares uint32)
+      -- (symbol uint64)
+      -- (price uint32)
+      -- (attribution uint32))
 
 
-__ 1.4 Modify Order Messages
-    __ 1.4.1 Order ExecutedMessage
+-- 1.4 Modify Order Messages
+    -- 1.4.1 Order ExecutedMessage
     when E =>
       stock_locate
       tracking_num
@@ -218,16 +232,16 @@ __ 1.4 Modify Order Messages
 
 
 
-      __ (69 order_executed ; E
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64)
-      __ (num_shares uint32)
-      __ (match_number uint64))
+      -- (69 order_executed ; E
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64)
+      -- (num_shares uint32)
+      -- (match_number uint64))
 
 
-    __ 1.4.2 Order Executed With PriceMessage
+    -- 1.4.2 Order Executed With PriceMessage
     when C =>
       stock_locate
       tracking_num
@@ -239,18 +253,18 @@ __ 1.4 Modify Order Messages
       exec_price
 
 
-      __ (67 order_executed_with_price ; C
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64)
-      __ (num_shares uint32)
-      __ (match_number uint64)
-      __ (printable uint8)
-      __ (price uint32))
+      -- (67 order_executed_with_price ; C
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64)
+      -- (num_shares uint32)
+      -- (match_number uint64)
+      -- (printable uint8)
+      -- (price uint32))
 
 
-    __ 1.4.3 Order Cancel Message
+    -- 1.4.3 Order Cancel Message
     when X =>
       stock_locate
       tracking_num
@@ -258,17 +272,17 @@ __ 1.4 Modify Order Messages
       order_ref_num
       num_share_cancel
 
-      __ exec_num_shares they shared signal
+      -- exec_num_shares they shared signal
 
-      __ (88 order_cancel ; X
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64)
-      __ (num_shares uint32))
+      -- (88 order_cancel ; X
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64)
+      -- (num_shares uint32))
 
 
-    __ 1.4.4 Order Delete Message
+    -- 1.4.4 Order Delete Message
     when D =>
       stock_locate
       tracking_num
@@ -276,61 +290,61 @@ __ 1.4 Modify Order Messages
       order_ref_num
 
 
-      __ (68 order_delete ; D
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64))
+      -- (68 order_delete ; D
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64))
 
 
-    __ 1.4.5. Order Replace Message
+    -- 1.4.5. Order Replace Message
     when U =>
       stock_locate
       tracking_num
       timestamp
       prev_order_ref_num
-      order_ref_num    __new one
+      order_ref_num    --new one
       exec_num_shares
       exec_price
 
-      __ (85 order_replace ; U
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (prev_order_ref_number uint64)
-      __ (order_ref_number uint64)
-      __ (num_shares uint32)
-      __ (price uint32))
+      -- (85 order_replace ; U
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (prev_order_ref_number uint64)
+      -- (order_ref_number uint64)
+      -- (num_shares uint32)
+      -- (price uint32))
 
 
 
-__ 1.5 Trade Messages
-    __ 1.5.1 Trade Message (Non___Cross)
+-- 1.5 Trade Messages
+    -- 1.5.1 Trade Message (Non--_Cross)
     when P =>
       stock_locate
       tracking_num
       timestamp
-      order_ref_num    __new one
+      order_ref_num    --new one
       buy_sell_ind
       exec_num_shares
       stock_symbol
       exec_price
       match_num
 
-      __ (80 trade ; P
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (order_ref_number uint64)
-      __ (buy_sell uint8)
-      __ (num_shares uint32)
-      __ (symbol uint64)
-      __ (price uint32)
-      __ (match_number uint64))
+      -- (80 trade ; P
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (order_ref_number uint64)
+      -- (buy_sell uint8)
+      -- (num_shares uint32)
+      -- (symbol uint64)
+      -- (price uint32)
+      -- (match_number uint64))
 
 
 
-    __ 1.5.2 Cross Trade Message
+    -- 1.5.2 Cross Trade Message
     when Q =>
       stock_locate
       tracking_num
@@ -341,36 +355,36 @@ __ 1.5 Trade Messages
       match_num
       cross_type
 
-      __ (81 cross_trade ; Q
-      __ (stock_locate uint16)
-      __ (tracking uint16)
-      __ (timestamp uint48)
-      __ (num_shares_msb uint32)
-      __ (num_shares uint32)
-      __ (price uint32)
-      __ (match_number uint64)
-      __ (cross_type uint8)
+      -- (81 cross_trade ; Q
+      -- (stock_locate uint16)
+      -- (tracking uint16)
+      -- (timestamp uint48)
+      -- (num_shares_msb uint32)
+      -- (num_shares uint32)
+      -- (price uint32)
+      -- (match_number uint64)
+      -- (cross_type uint8)
 
 
 
       
-__ __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.5.3 Broken Trade / Order ExecutionMessage
-__     when B =>
-__       code for this branch
+-- -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.5.3 Broken Trade / Order ExecutionMessage
+--     when B =>
+--       code for this branch
 
 
-__ __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.6 Net Order Imbalance Indicator (NOII)Message
-__     when I =>
-__       code for this branch
+-- -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.6 Net Order Imbalance Indicator (NOII)Message
+--     when I =>
+--       code for this branch
 
-__     __ 1.7 was grayed out
+--     -- 1.7 was grayed out
 
-__ __ i dont know if we need this, they didn't have it. what determines what we need.
-__     __ 1.8 Direct Listing with Capital Raise Price Discovery Message
-__     when O =>
-__       code for this branch
+-- -- i dont know if we need this, they didn't have it. what determines what we need.
+--     -- 1.8 Direct Listing with Capital Raise Price Discovery Message
+--     when O =>
+--       code for this branch
 
   when others =>
 
@@ -445,11 +459,9 @@ end case;
 -- Computes the global message seqnum for each message
 (def_counter seqnum32 32 :increment msg_type_sync :enable ethernet_input_tvalid :clear (delay seqnum_sync 3) :reset_value seqnum)
 
--- __ this is confusing =>  why?
--- Only accepts the packets which don't have the correct IP addresse and port
-(setf (var bit packet_ok) (bit.and (= ip_dest_addr nasdaq_ip_addr) (= udp_dest_port nasdaq_udp_port)))
 
--- __ not sure im going to do this, since i wasn't putting into axi stream
+
+-- -- not sure im going to do this, since i wasn't putting into axi stream
 -- Stores various event codes into num_shares to reduce the AXI stream width
 (setf num_shares (case_expr msg_type
                             (83 event_code)
@@ -468,7 +480,20 @@ end case;
                                 seqnum32
                                 timestamp))
 
+-- -- this is confusing =>  why?
+-- Only accepts the packets which don't have the correct IP addresse and port
+(setf (var bit packet_ok) (bit.and (= ip_dest_addr nasdaq_ip_addr) (= udp_dest_port nasdaq_udp_port)))
+the comment is deceptive, the code seems to actually be =>  if ip address and udp port match whateve we wrote at the top,
+assert packet_ok, whic his what i would think it should be
 
 (setf command_out_tvalid (bit.and packet_ok
                                   (bit.or price_sync event_code_sync trading_state_sync reg_sho_action_sync
                                           (bit.and (bit.or (= msg_type 69) (= msg_type 88)) num_shares_sync))))))
+
+if packet_ok and [
+[price_sync or event_code_sync or trading_state_sync or reg_sho_action_sync or
+[ [ msg69 or msg88] and num_shares_sync ]
+]
+
+i assume sync is just for registering these signals? but that doesn't make sense because those values up there aren't flags,
+price is not a flag nore is event code?
